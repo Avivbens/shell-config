d614abfe2a97148a90f0763ba9d1989fdc328ed4
Aviv Ben Shahar <avivbens87@gmail.com>
2023-08-16 01:11:32 +0300

    add hosts at pre-init scripts

--- src/scripts/pre-init.sh
+++ src/scripts/pre-init.sh
@@ -1,11 +1,7 @@
 echo -e "\e[33mInstall Xcode Command Line Tools\e[0m"
 softwareupdate -i "Command Line Tools for Xcode-13.3" --agree-to-license
 
-
-# echo -e "\e[33mAdd Homebrew to environment\e[0m"
-# echo "eval \"$(/opt/homebrew/bin/brew shellenv)\"" >> ~/Desktop/.zshrc
-# eval "$(/opt/homebrew/bin/brew shellenv)"
-
-
 echo -e "\e[33mInstall Homebrew\e[0m"
 yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+sudo chmod +a "user:$USER allow read,write,append,readattr,writeattr,readextattr,writeextattr,readsecurity" /etc/hosts && echo 127.0.0.1 dev.zoominfo.com >> /etc/hosts
