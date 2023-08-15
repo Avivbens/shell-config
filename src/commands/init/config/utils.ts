59abfa868d49d7bd3fc3782d6ecdb8238a867732
Aviv Ben Shahar <avivbens87@gmail.com>
2023-08-15 17:30:06 +0300

    fix npm global registry

--- src/commands/init/config/utils.ts
+++ src/commands/init/config/utils.ts
@@ -1,4 +1,5 @@
 export const BREW_CASK = (app: string) => `brew install --cask ${app}`
 export const BREW_INSTALL = (formula: string) => `brew install ${formula}`
 export const BREW_TAP = (tapTo: string) => `brew tap ${tapTo}`
-export const NODE_GLOBAL = (packageName: string) => `npm install -g ${packageName}`
+export const NODE_GLOBAL = (packageName: string) =>
+    `npm install -g ${packageName} --registry=https://registry.npmjs.org/`
