ea2f028782caaa1715fae34020cd68ae46e9fa49
Aviv Ben Shahar <avivbens87@gmail.com>
2023-08-15 18:25:16 +0300

    remove git clone target for zsh terminal extention

--- src/commands/init/config/apps-groups/terminal.ts
+++ src/commands/init/config/apps-groups/terminal.ts
@@ -13,9 +13,9 @@ export const TERMINAL_APPS: Readonly<IAppSetup[]> = [
         group: 'terminal',
         default: true,
         commands: [
-            'git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions',
-            'git clone https://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions',
-            'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting',
+            'rm -rf ~/.zsh/zsh-autosuggestions && git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions',
+            'rm -rf ~/.zsh/zsh-completions && git clone https://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions',
+            'rm -rf ~/.zsh/zsh-syntax-highlighting && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting',
         ],
     },
 ] as const
