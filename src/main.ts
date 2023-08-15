cab7577cf9ad2817a58056f7e5ca2c918fca1a4a
Aviv Ben Shahar <avivbens87@gmail.com>
2023-08-16 02:45:34 +0300

    remove console error on global error handler

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module'
 ;(async () => {
     await CommandFactory.run(AppModule, {
         errorHandler: (err) => {
-            console.error(err)
             process.exit(1)
         },
     })
