c14b41bdccc86f725b777f18c0b3aac5ce30c554
Aviv Ben Shahar <avivbens87@gmail.com>
2023-08-17 20:39:34 +0300

    feat: git apps

--- src/commands/init/config/apps.config.ts
+++ src/commands/init/config/apps.config.ts
@@ -1,8 +1,10 @@
 import { IAppSetup } from 'src/models/app-setup.model'
 import { APPS, CLI_APPS, MACOS, NODE_APPS, PYTHON, TERMINAL_APPS, ZI } from './apps-groups'
+import { GIT_APPS } from './apps-groups/git'
 
 export const APPS_CONFIG: Readonly<IAppSetup[]> = [
     ...ZI,
+    ...GIT_APPS,
     ...APPS,
     ...TERMINAL_APPS,
     ...CLI_APPS,
