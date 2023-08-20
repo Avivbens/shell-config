b654b52a085d4a2e9dd743fb3c4a2a5405a78fec
Aviv Ben Shahar <avivbens87@gmail.com>
2023-08-20 15:40:31 +0300

    fix: using base_path for all access to home/shell-config

--- src/commands/update/config/update-script.config.ts
+++ src/commands/update/config/update-script.config.ts
@@ -1,21 +1,37 @@
-export const DOWNLOAD_SCRIPT = `
-curl -s https://api.github.com/repos/Avivbens/shell-config/releases/latest \
+import { BASE_PATH } from '@common/constants'
+
+export const DOWNLOAD_SCRIPT_LATEST = `
+curl -s "https://api.github.com/repos/Avivbens/shell-config/releases/latest" \
 | grep "browser_download_url.*cli-v.*.zip" \
 | cut -d : -f 2,3 \
 | tr -d \" \
-| xargs curl -L -A "Mozilla/5.0" -o $HOME/shell-config/downloads/cli-update.zip
+| xargs curl -L -A "Mozilla/5.0" -o "${BASE_PATH}/downloads/cli-update.zip"
+`
+
+/**
+ * @param version - target version
+ * @example
+ * ```typescript
+ * DOWNLOAD_SCRIPT_CUSTOM('v1.1.1')
+ * ```
+ * @returns a bash command to download the target version
+ */
+export const DOWNLOAD_SCRIPT_CUSTOM = (version: string) => `
+curl -L "https://github.com/Avivbens/shell-config/releases/download/${version}/cli-${version}.zip" -s -A "Mozilla/5.0" -o "${BASE_PATH}/downloads/cli-update.zip"
 `
 
 export const UNZIP_SCRIPT = `
-unzip $HOME/shell-config/downloads/cli-update.zip -d $HOME/shell-config/downloads
-filename=$(basename $HOME/shell-config/downloads/bin/*)
-mv $HOME/shell-config/downloads/bin/* $HOME/shell-config/downloads
+unzip "${BASE_PATH}/downloads/cli-update.zip" -d "${BASE_PATH}/downloads"
+filename="$(basename ${BASE_PATH}/downloads/bin/*)"
+mv "${BASE_PATH}/downloads/bin/*" "${BASE_PATH}/downloads"
 echo $filename
 
-rm $HOME/shell-config/downloads/cli-update.zip
-rm -rf $HOME/shell-config/downloads/bin
+rm "${BASE_PATH}/downloads/cli-update.zip"
+rm -rf "${BASE_PATH}/downloads/bin"
 `
 
-export const UPDATE_SCRIPT = `
-ln -f "$HOME/shell-config/downloads/$filename" "$HOME/shell-config/executable/shell-config"
+export const DOWNLOAD_FILE_PATH = (filename: string) =>
+    `${BASE_PATH}/downloads/${filename.replace(/\n/g, '')}`
+export const UPDATE_SCRIPT = (filename: string) => `
+ln -f "${DOWNLOAD_FILE_PATH(filename)}" "${BASE_PATH}/executable/shell-config"
 `
