8ef4e71cf166345a070e74097e6352e578706e68
Aviv Ben Shahar <avivbens87@gmail.com>
2023-08-21 02:23:40 +0300

    feat: init command to unpack files and link zshrc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { HttpModule } from '@nestjs/axios'
 import { Module } from '@nestjs/common'
+import { InitCommand } from './commands/init/init.command'
 import { InstallCommand } from './commands/install/install.command'
 import { ShellCommand } from './commands/shell/shell.command'
 import { UpdateCommand } from './commands/update/update.command'
 import { CheckUpdateService } from './services/check-update.service'
 import { LoggerService } from './services/logger.service'
 
-const COMMANDS = [InstallCommand, ShellCommand, UpdateCommand]
+const COMMANDS = [InitCommand, InstallCommand, ShellCommand, UpdateCommand]
 
 @Module({
     imports: [HttpModule],
---
+++ src/commands/init/init.command.ts
@@ -0,0 +1,89 @@
+import { BASE_PATH } from '@common/constants'
+import { copyBundledAsset, resolveBundledAsset } from '@common/utils'
+import { LoggerService } from '@services/logger.service'
+import { Command, CommandRunner } from 'nest-commander'
+import { copyFile, mkdir, symlink } from 'node:fs/promises'
+import { homedir } from 'node:os'
+import { resolve } from 'node:path'
+
+@Command({
+    name: 'init',
+    description:
+        'Init shell-config setup with this CLI version. Execute this command only when you want to force-apply the latest shell-config setup',
+    options: { isDefault: false },
+})
+export class InitCommand extends CommandRunner {
+    constructor(private readonly logger: LoggerService) {
+        super()
+    }
+
+    async run(inputs: string[], options: Record<string, any>): Promise<void> {
+        try {
+            // const isBackupRoot: boolean = await BACKUP_ROOT_ZSH_CONFIRM_PROMPT()
+
+            // if (isBackupRoot) {
+            // }
+
+            await this.backupRootZsh()
+
+            await this.unpackBundledAssets()
+
+            await this.linkNewZsh()
+        } catch (error) {
+            this.logger.error(`Failed to init shell-config CLI, error: ${error.stack}`)
+        }
+    }
+
+    private async backupRootZsh(): Promise<void> {
+        try {
+            const rootZshPath = resolve(homedir(), '.zshrc')
+            const backupRootZshPath = resolve(homedir(), 'Desktop', `zshrc.backup-${Date.now()}`)
+
+            this.logger.debug(`Backing up root zsh at ${rootZshPath} to ${backupRootZshPath}`)
+
+            await copyFile(rootZshPath, backupRootZshPath)
+        } catch (error) {
+            this.logger.error(`Failed to backup root zsh: ${error.stack}`)
+        }
+    }
+
+    private async linkNewZsh(): Promise<void> {
+        try {
+            const zshPath = resolve(homedir(), '.zshrc')
+            const newZshPath = resolve(BASE_PATH, 'zsh', '.zshrc')
+
+            this.logger.debug(`Linking new zsh at ${newZshPath} to ${zshPath}`)
+            await symlink(newZshPath, zshPath)
+        } catch (error) {
+            this.logger.error(`Failed to link new zsh: ${error.stack}`)
+        }
+    }
+
+    private async unpackBundledAssets(): Promise<void> {
+        try {
+            this.logger.debug(`Unpacking bundled assets to ${BASE_PATH}`)
+            const prmsMake = [
+                mkdir(`${BASE_PATH}/zsh`, { recursive: true }),
+                mkdir(`${BASE_PATH}/private`, { recursive: true }),
+            ]
+            await Promise.allSettled(prmsMake)
+
+            this.logger.debug(`Copying bundled assets to ${BASE_PATH}`)
+
+            const privatePath = resolveBundledAsset(__dirname, 'private/')
+            const zshPath = resolveBundledAsset(__dirname, 'zsh/')
+
+            const prmsCopy = [
+                copyBundledAsset(privatePath, `${BASE_PATH}/private`, this.logger),
+                copyBundledAsset(zshPath, `${BASE_PATH}/zsh`, this.logger),
+            ]
+
+            await Promise.all(prmsCopy)
+
+            this.logger.debug(`Finished unpacking bundled assets to ${BASE_PATH}`)
+        } catch (error) {
+            this.logger.error(`Failed unpacking bundled assets, error: ${error.stack}`)
+            throw error
+        }
+    }
+}
