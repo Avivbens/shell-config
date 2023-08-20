02630faeb8a547fd0302056ac8c70edf8e660a6d
Aviv Ben Shahar <avivbens87@gmail.com>
2023-08-20 15:50:59 +0300

    fix: show version on version flag

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { CommandFactory } from 'nest-commander'
 import { AppModule } from './app.module'
+
+const packageJson = require('../package.json')
+
 ;(async () => {
     const app = await CommandFactory.createWithoutRunning(AppModule, {
         errorHandler: (err) => {
             process.exit(1)
         },
+        version: packageJson.version,
     })
 
     app.enableShutdownHooks()
