fb68d34a808d7cc6945d01990bc4b700e2267833
Aviv Ben Shahar <avivbens87@gmail.com>
2023-08-24 13:13:16 +0300

    fix: change imports of commands to use paths

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
+import { ExternalCommand } from '@commands/external/external.command'
+import { InstallCommand } from '@commands/external/sub-commands'
+import { InitCommand } from '@commands/init/init.command'
+import { ShellCommand } from '@commands/shell/shell.command'
+import { UpdateCommand } from '@commands/update/update.command'
 import { HttpModule } from '@nestjs/axios'
 import { Module } from '@nestjs/common'
-import { InitCommand } from './commands/init/init.command'
-import { InstallCommand } from './commands/install/install.command'
-import { ShellCommand } from './commands/shell/shell.command'
-import { UpdateCommand } from './commands/update/update.command'
 import { CheckUpdateService } from './services/check-update.service'
 import { LoggerService } from './services/logger.service'
 
-const COMMANDS = [InitCommand, InstallCommand, ShellCommand, UpdateCommand]
+const COMMANDS = [
+    InitCommand,
+    InstallCommand,
+    ShellCommand,
+    UpdateCommand,
+    ...ExternalCommand.registerWithSubCommands(),
+]
 
 @Module({
     imports: [HttpModule],
