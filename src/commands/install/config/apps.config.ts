1e902a29abeb79337c43a1f338e988f12153bb0b
Aviv Ben Shahar <avivbens87@gmail.com>
2023-08-27 15:24:09 +0300

    fix: remove hosts setup for ZI in install command

--- src/commands/install/config/apps.config.ts
+++ src/commands/install/config/apps.config.ts
@@ -1,9 +1,8 @@
 import { IAppSetup } from '@models/app-setup.model'
-import { APPS, CLI_APPS, MACOS, NODE_APPS, PYTHON, TERMINAL_APPS, ZI } from './apps-groups'
+import { APPS, CLI_APPS, MACOS, NODE_APPS, PYTHON, TERMINAL_APPS } from './apps-groups'
 import { GIT_APPS } from './apps-groups/git'
 
 export const APPS_CONFIG: Readonly<IAppSetup[]> = [
-    ...ZI,
     ...GIT_APPS,
     ...APPS,
     ...TERMINAL_APPS,
