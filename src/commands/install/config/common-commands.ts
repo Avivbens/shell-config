681bdda21a8b7a444efccc4b29c95ef5cc232ca6
Aviv Ben Shahar <avivbens87@gmail.com>
2023-08-27 18:30:04 +0300

    fix: setup yes command for each installation command

--- src/commands/install/config/common-commands.ts
+++ src/commands/install/config/common-commands.ts
@@ -1,6 +1,8 @@
-export const BREW_CASK = (app: string) => `HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask ${app}`
-export const BREW_INSTALL = (formula: string) => `HOMEBREW_NO_AUTO_UPDATE=1 brew install ${formula}`
-export const BREW_TAP = (tapTo: string) => `HOMEBREW_NO_AUTO_UPDATE=1 brew tap ${tapTo}`
+export const BREW_CASK = (app: string) =>
+    `yes | HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask ${app}`
+export const BREW_INSTALL = (formula: string) =>
+    `yes | HOMEBREW_NO_AUTO_UPDATE=1 brew install ${formula}`
+export const BREW_TAP = (tapTo: string) => `yes | HOMEBREW_NO_AUTO_UPDATE=1 brew tap ${tapTo}`
 export const NODE_GLOBAL = (packageName: string) =>
     `npm install -g ${packageName} --registry=https://registry.npmjs.org/`
 
