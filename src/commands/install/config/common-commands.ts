5a0e76cd6d7d2f9807259111d9d654767e86e26b
Aviv Ben Shahar <avivbens87@gmail.com>
2023-08-27 19:10:53 +0300

    fix: add NONINTERACTIVE flag to homebrew

--- src/commands/install/config/common-commands.ts
+++ src/commands/install/config/common-commands.ts
@@ -1,8 +1,9 @@
 export const BREW_CASK = (app: string) =>
-    `yes | HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask ${app}`
+    `yes | HOMEBREW_NO_AUTO_UPDATE=1 NONINTERACTIVE=1 brew install --cask ${app}`
 export const BREW_INSTALL = (formula: string) =>
-    `yes | HOMEBREW_NO_AUTO_UPDATE=1 brew install ${formula}`
-export const BREW_TAP = (tapTo: string) => `yes | HOMEBREW_NO_AUTO_UPDATE=1 brew tap ${tapTo}`
+    `yes | HOMEBREW_NO_AUTO_UPDATE=1 NONINTERACTIVE=1 brew install ${formula}`
+export const BREW_TAP = (tapTo: string) =>
+    `yes | HOMEBREW_NO_AUTO_UPDATE=1 NONINTERACTIVE=1 brew tap ${tapTo}`
 export const NODE_GLOBAL = (packageName: string) =>
     `npm install -g ${packageName} --registry=https://registry.npmjs.org/`
 
