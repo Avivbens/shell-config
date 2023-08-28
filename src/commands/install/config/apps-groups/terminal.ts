4cd25e83e814bf49ab9747c8a2bcaf7def9c8f60
Aviv Ben Shahar <avivbens87@gmail.com>
2023-08-28 16:14:07 +0300

    fix: load nvm with brew internal after install

--- src/commands/install/config/apps-groups/terminal.ts
+++ src/commands/install/config/apps-groups/terminal.ts
@@ -10,7 +10,7 @@ export const TERMINAL_APPS: Readonly<IAppSetup[]> = [
         commands: [
             BREW_INSTALL('nvm'),
             'sleep 5',
-            `eval "$(/opt/homebrew/bin/brew shellenv)"`,
+            `$(brew --prefix)/opt/nvm/nvm.sh`,
             `chmod +x $HOME/.nvm/nvm.sh`,
             NVM_COMMAND('install 16.14.0'),
             NVM_COMMAND('alias default 16.14.0'),
@@ -18,7 +18,7 @@ export const TERMINAL_APPS: Readonly<IAppSetup[]> = [
         commandsFallback: [
             BREW_INSTALL_64_ARM('nvm'),
             'sleep 5',
-            `eval "$(/opt/homebrew/bin/brew shellenv)"`,
+            `$(brew --prefix)/opt/nvm/nvm.sh`,
             `chmod +x $HOME/.nvm/nvm.sh`,
             NVM_COMMAND('install 16.14.0'),
             NVM_COMMAND('alias default 16.14.0'),
