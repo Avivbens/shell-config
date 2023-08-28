5b78262ba9b73d6507312dc87929605356fb6a1c
Aviv Ben Shahar <avivbens87@gmail.com>
2023-08-28 15:19:02 +0300

    fix: apply sleep for installing nvm, wait for creating files

--- src/commands/install/config/apps-groups/terminal.ts
+++ src/commands/install/config/apps-groups/terminal.ts
@@ -9,12 +9,14 @@ export const TERMINAL_APPS: Readonly<IAppSetup[]> = [
         default: true,
         commands: [
             BREW_INSTALL('nvm'),
+            'sleep 5',
             `chmod +x $HOME/.nvm/nvm.sh`,
             NVM_COMMAND('install 16.14.0'),
             NVM_COMMAND('alias default 16.14.0'),
         ],
         commandsFallback: [
             BREW_INSTALL_64_ARM('nvm'),
+            'sleep 5',
             `chmod +x $HOME/.nvm/nvm.sh`,
             NVM_COMMAND('install 16.14.0'),
             NVM_COMMAND('alias default 16.14.0'),
