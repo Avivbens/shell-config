74a4e1e6a49155f60bcdecfa02267f0637ec12bc
Aviv Ben Shahar <avivbens87@gmail.com>
2023-09-03 12:55:58 +0300

    feat: support for mcfly

--- zsh/.entry-point.sh
+++ zsh/.entry-point.sh
@@ -59,6 +59,10 @@ setopt correct_all # autocorrect commands
 setopt auto_list # automatically list choices on ambiguous completion
 setopt auto_menu # automatically use menu completion
 setopt always_to_end # move cursor to end if word had one match
+if command -v mcfly &> /dev/null
+then
+    eval "$(mcfly init zsh)"
+fi
 
 
 # general
