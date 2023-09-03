216862827363d4e7bae11e2ce384e91749be036e
Aviv Ben Shahar <avivbens87@gmail.com>
2023-09-04 01:34:54 +0300

    fix: set completion over commandFactory

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,23 @@
-import { CommandFactory, CompletionFactory } from 'nest-commander'
+import { CommandFactory } from 'nest-commander'
 import { AppModule } from './app.module'
 
 const packageJson = require('../package.json')
 
 ;(async () => {
+    const cliCommand = 'shell-config'
+    const cliExecutable = '$HOME/shell-config/executable/shell-config'
+
     const app = await CommandFactory.createWithoutRunning(AppModule, {
         errorHandler: (err) => {
             process.exit(1)
         },
         version: packageJson.version,
-    })
-
-    const cliCommand = 'shell-config'
-    const cliExecutable = '$HOME/shell-config/executable/shell-config'
-
-    CompletionFactory.registerCompletionCommand(app, {
-        cmd: cliCommand,
-        fig: true,
-        nativeShell: {
-            executablePath: cliExecutable,
+        completion: {
+            cmd: cliCommand,
+            fig: true,
+            nativeShell: {
+                executablePath: cliExecutable,
+            },
         },
     })
 
