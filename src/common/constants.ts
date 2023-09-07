60bf988c246d4992c520b1bcb4928d92698a911d
Aviv Ben Shahar <avivbens87@gmail.com>
2023-09-07 04:14:48 +0300

    fix: support instance id as part of logger

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,6 +1,7 @@
+import { randomUUID } from 'node:crypto'
 import { homedir } from 'node:os'
 
 export const GITHUB_REPO = 'Avivbens/shell-config'
 export const GITHUB_RELEASES_API_URL = `https://api.github.com/repos/${GITHUB_REPO}/releases`
 export const BASE_PATH = `${homedir()}/shell-config`
-export const BOOTSTRAP_TIME = new Date().toISOString()
+export const BOOTSTRAP_UUID = randomUUID().slice(0, 8)
