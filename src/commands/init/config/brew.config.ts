5de6db3d4f658b0aefe340422d39658c9a64fe18
Aviv Ben Shahar <avivbens87@gmail.com>
2023-09-25 03:15:08 +0300

    fix: setup ci in order to install brew as sudo

--- src/commands/init/config/brew.config.ts
+++ src/commands/init/config/brew.config.ts
@@ -2,8 +2,12 @@ export const BREW_DIRECTORY = `/opt/homebrew/bin/brew`
 export const BROW_DIRECTORY = `/usr/local/Homebrew/bin/brew`
 
 export const BREW_INSTALLATION_COMMAND = `
+export HOMEBREW_ON_LINUX=true
+export CI=true
 NONINTERACTIVE=1 yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 `
 export const BROW_INSTALLATION_COMMAND = `
+export HOMEBREW_ON_LINUX=true
+export CI=true
 NONINTERACTIVE=1 yes | arch --x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 `
