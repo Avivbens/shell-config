9e3f167fb9f219ba0b17166e67b842817ffdc983
Aviv Ben Shahar <avivbens87@gmail.com>
2024-05-03 00:13:01 +0300

    feat(apps): support `Sudo with Touch ID` feature

--- src/commands/install/config/apps-groups/mac.ts
+++ src/commands/install/config/apps-groups/mac.ts
@@ -2,6 +2,16 @@ import type { IAppSetup } from '@models/app-setup.model'
 
 export const MACOS: Readonly<IAppSetup[]> = [
     {
+        name: 'Sudo password with Touch ID',
+        group: 'MacOS',
+        default: true,
+        commands: () => [
+            'sudo -v',
+            'sudo cp -f /etc/pam.d/sudo_local.template /etc/pam.d/sudo_local',
+            `sudo /bin/bash -c "echo 'auth       sufficient     pam_tid.so' >> /etc/pam.d/sudo_local"`,
+        ],
+    },
+    {
         name: 'Update MacOS version',
         group: 'MacOS',
         default: true,
