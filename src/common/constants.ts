f7c273a31f8ba092c915661164fe51fdb09ff2f0
Aviv Ben Shahar <avivbens87@gmail.com>
2024-05-20 14:50:46 +0300

    chore(tests): fix broken tests, import as default `package.json`

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,7 +1,7 @@
 import { randomUUID } from 'node:crypto'
 import { homedir } from 'node:os'
 import { stdout } from 'node:process'
-import packageJson from '../../package.json'
+import * as packageJson from '../../package.json'
 
 export const GITHUB_REPO = 'Avivbens/shell-config' as const
 export const GITHUB_RELEASES_API_URL = `https://api.github.com/repos/${GITHUB_REPO}/releases` as const
